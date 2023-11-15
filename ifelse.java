60b9be2adbbf9986e7d8c448e4eaa91b62ff90c2
sravanineelam <107420776+sravanineelam@users.noreply.github.com>
2023-11-15 16:17:37 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,17 @@
+package selections;
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		int a=2,b=33,c=20;
+		if((a<b)&&(a>c)) {
+			System.out.println("a is big");
+		}else {
+			System.out.println("b is big");
+			
+				} 
+			System.out.println("prg end");
+
+	}
+
+}
