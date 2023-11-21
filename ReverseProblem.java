09575a23b3c1c4cf1317e24f159269f1c303e509
sravanineelam <107420776+sravanineelam@users.noreply.github.com>
2023-11-21 16:38:57 +0530

    Add files via upload

--- ReverseProblem.java
+++ ReverseProblem.java
@@ -4,7 +4,7 @@ public class ReverseProblem {
 	public static void main(String[] args) {
 		
 		for(int i=1;i<=5;i++) {
-			for(int j=1;j<=i;j++) {
+			for(int j=5;j>=i;j--) {
 				System.out.print("5");
 		}
 System.out.println();
