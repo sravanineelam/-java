d564c128bcdf9fe9fe5dc6fa2b96abfc19cfffc4
sravanineelam <107420776+sravanineelam@users.noreply.github.com>
2023-11-23 16:09:36 +0530

    Add files via upload

---
+++ NoargsNoreturn.java
@@ -0,0 +1,26 @@
+package Method;
+
+public class NoargsNoreturn {
+
+	
+		// methods-- 4 types
+		// no args ...no return
+		
+		static void design(){
+			System.out.println("2");
+			System.out.println("4");
+		}
+		public static void main(String[] args) {
+			
+			design();
+			System.out.println("Hello");
+			design();
+			System.out.println("Hi");
+			
+		}
+
+	
+
+	}
+
+
