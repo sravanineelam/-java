11d1f5e87ba70a0812d15482ee98f9e84253c412
sravanineelam <107420776+sravanineelam@users.noreply.github.com>
2023-11-24 10:29:07 +0530

    Add files via upload

---
+++ printPrimeNumbers.java
@@ -0,0 +1,30 @@
+package Method;
+
+public class printPrimeNumbers {
+	static boolean isPrime(int num){
+		int count =0;
+		for(int i=1;i<=num;i++) {
+			if(num % i == 0) count++;
+		}
+		if(count == 2) return true;
+		else  return false;
+	}
+	
+	static void printPrimeNumbers(int start,int end){
+		
+		for(int i=start;i<=end;i++) {
+			
+			if(isPrime(i)) {
+				System.out.println(i);
+			}
+			
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+	
+		printPrimeNumbers(50,100);
+		
+}
+}
