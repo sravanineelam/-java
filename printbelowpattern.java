8726775a64247e4b993241aadc1517c9508490fd
sravanineelam <107420776+sravanineelam@users.noreply.github.com>
2023-11-28 16:23:39 +0530

    Add files via upload

---
+++ printbelowpattern.java
@@ -0,0 +1,19 @@
+package exam;
+
+public class printbelowpattern {
+	//write a program to print below pattern
+	//1 2 3 4 5  
+	//1 2 3 4
+	//1 2 3
+	//1 2
+	//1
+	public static void main(String[] args) {
+		for(int i=5;i>=1;i--) {
+			for(int j=1;j<=i;j++) {
+				 System.out.print(j+" ");
+		}
+          System.out.println();
+	}
+
+}
+}
