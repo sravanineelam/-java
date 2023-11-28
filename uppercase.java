4932d2c75e81340a974b660082ddd684887a3699
sravanineelam <107420776+sravanineelam@users.noreply.github.com>
2023-11-28 16:33:23 +0530

    Add files via upload

---
+++ uppercase.java
@@ -0,0 +1,13 @@
+package exam;
+
+public class uppercase {
+	//write a java program to print the given string in uppercase
+	public static void main(String[] args) {
+		
+       String str="sravani";
+      String newstr=str.toUpperCase();
+      System.out.println(newstr);
+       }
+	}
+
+
