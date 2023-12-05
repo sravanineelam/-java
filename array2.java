9b2420b2aae5da0a6dc1132e38154f712c6482ee
sravanineelam <107420776+sravanineelam@users.noreply.github.com>
2023-12-05 16:11:14 +0530

    Add files via upload

---
+++ array2.java
@@ -0,0 +1,21 @@
+package array;
+
+public class array2 {
+
+	public static void main(String[] args) {
+int a1,a2,a3,a4;
+int[]n= {12,13,21,34,54,
+		12,16,41,94,54,
+		42,13,61,4,54};
+System.out.println("total elements--->"+n.length);
+	
+for(int i=1;i<n.length;i++)
+System.out.println("n["+i+"]-->"+n[i]);
+
+
+
+
+
+
+}
+}
