77db1d0381a0fd6134a076e82c6a3aeb17c97362
sravanineelam <107420776+sravanineelam@users.noreply.github.com>
2023-12-05 15:49:08 +0530

    Add files via upload

---
+++ array1.java
@@ -0,0 +1,18 @@
+package array;
+
+public class array1 {
+
+	public static void main(String[] args) {
+		int a1,a2,a3,a4,a5,a6;
+		int[]n=new int[6];
+		n[0]=1;
+		n[1]=2;
+		n[2]=3;
+		n[3]=4;
+		for(int i=0;i<5;i++) {
+		System.out.println("n["+i+"]---->"+n[i]);
+
+	}
+
+}
+}
