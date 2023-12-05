2fb19eae9515754e6348fd7963702f76fbd80499
sravanineelam <107420776+sravanineelam@users.noreply.github.com>
2023-12-05 15:49:27 +0530

    Add files via upload

---
+++ array3.java
@@ -0,0 +1,22 @@
+package array;
+
+public class array3 {
+
+	public static void main(String[] args) {
+		char ch[];
+		ch=new char[6];
+		ch[0]='0';
+		ch[1]='s';
+		ch[2]='e';
+		ch[3]='3';
+		ch[4]='r';
+        ch[5]='9';
+        for(int i=0;i<ch.length;i++) {
+        	System.out.println(ch[i]+" ");
+        }
+
+		
+
+	}
+
+}
